d7f261daa95a03e54dbd49ce3b3c7db8021a5df1
ArturoBL <dark.laker@gmail.com>
2025-05-27 22:41:05 -0600

    FormParamaters example added

--- Flask/FormParams/formparams.py
+++ Flask/FormParams/formparams.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 # Get params from form data
 # test from command line: curl -X POST http://localhost:5000/login -d "user=admin&Password=1234"
+# Or test from form 
+'''
+<form action="http://localhost:5000/login" method="post">
+  <input type="text" name="user">
+  <input type="password" name="password">
+  <button type="submit">Ingresar</button>
+</form>
+'''
 @app.route('/login', methods=['POST'])
 def login():
     usuario = request.form.get('user')
